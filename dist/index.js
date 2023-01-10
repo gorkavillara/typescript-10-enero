4d6684cbb75a9a08407dbdf4c3331289fe8baba9
gorkavillar <gorka@gorkavillar.com>
2023-01-10 12:04:44 +0100

    🔧 Configuración del proyecto finalizada

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+// CommonJS -> Bastante obsoleto
+// const chalk = require("chalk")
+// ESModules
+import chalk from 'chalk';
+console.log(chalk.cyan("Hola mundo desde TypeScript"));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// CommonJS -> Bastante obsoleto
+// const chalk = require("chalk")
+// ESModules
+import chalk from 'chalk'
+
+console.log(chalk.cyan("Hola mundo desde TypeScript"))
