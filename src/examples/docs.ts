d1e8035db2cb664b097127bd28c80eb91286c070
gorkavillar <gorka@gorkavillar.com>
2023-01-13 09:58:35 +0100

    📖 Documentación generada

---
+++ src/examples/docs.ts
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param a Primer parámetro
+ * @param b Segundo parámetro
+ * @returns La suma aritmética de a + b
+ */
+const suma = (a: number, b: number): number => a + b
+
+/**
+ * La función saluda, pasa por pantalla un saludo para el nombre indicado en el parámetro
+ * 
+ * @param nombre El nombre del usuario
+ */
+const saluda = (nombre: string): void => {
+    console.log(`Buenos días ${nombre}`)
+}
